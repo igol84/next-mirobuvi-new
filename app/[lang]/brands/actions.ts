a19d82a8017b227ac5920a5420d9a0e7019e4770
igol84 <igolub84@gmail.com>
2024-04-08 11:15:08 +0300

    fix

--- app/[lang]/brands/actions.ts
+++ app/[lang]/brands/actions.ts
@@ -13,11 +13,13 @@ export const serverActionCreateNeBrand = async (formData: FormData) => {
   const name_ua = formData.get('nameUa') as string
   const meta_desc_en = formData.get('metaDescEn') as string
   const meta_desc_ua = formData.get('metaDescUa') as string
+  const text_en = formData.get('textEn') as string
+  const text_ua = formData.get('textUa') as string
   const url = formData.get('url') as string
   const tags = formData.get('tags') as string
   const active = true
   const newBrandData: CreateBrandType = {
-    title_ua , url, active, tags, title_en, name_en, name_ua, meta_desc_en, meta_desc_ua
+    title_ua , url, active, tags, title_en, name_en, name_ua, meta_desc_en, meta_desc_ua, text_ua, text_en
   }
   await createBrand(newBrandData)
 }
