f27fc62b89012341669451775f308a61aa9c9541
igol84 <igolub84@gmail.com>
2024-05-29 10:42:25 +0300

    fix sorting

--- lib/server/serverFunctions.ts
+++ lib/server/serverFunctions.ts
@@ -29,7 +29,7 @@ export const getPageData: GetPageData = async (items, currentPage, withoutOnePro
 }
 
 export const sortingProducts = (products: ProductType[], sortingBy: SortingType): ProductType[] => {
-  const isInStock = (product: ProductType): boolean => !!product.name
+  const isInStock = (product: ProductType): boolean => product.isAvailable
   switch (sortingBy) {
     case "byOrder": {
       return _.orderBy(products, [isInStock, 'id'], ['desc', 'desc'])
